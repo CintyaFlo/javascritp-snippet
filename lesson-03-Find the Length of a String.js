901993af2620f06aa7f41a53816085645e02163b
CintyaFlo <flores.cd@gmail.com>
2022-09-18 17:44:19 -0300

    sum length os String

---
+++ lesson-03-Find the Length of a String.js
@@ -0,0 +1,16 @@
+// Setup
+let lastNameLength = 3;
+const lastName = "Lovelace";
+
+//Atribui à variavel lastastNameLength o tamanho da variável LastName usando a propriedade .length o resultado será 8
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+/*
+criei uma variavel e pedi que ela somasse o numero de caracteres de lasName 
+com a variavel LastNameLengh cujo valor já estava dado segundo a Length
+*/
+var characterSum = lastName.length + lastNameLength;
+
+// o resultado da impressao abaixo será 16
+console.log(characterSum);
