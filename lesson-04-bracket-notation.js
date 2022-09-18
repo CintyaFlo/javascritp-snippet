b3be2f89f2c83e41ee0564ddfe253ab6af6f3c00
CintyaFlo <flores.cd@gmail.com>
2022-09-18 19:38:40 -0300

    Find Nth Character

---
+++ lesson-04-bracket-notation.js
@@ -0,0 +1,18 @@
+// atribuindo variaveis para encontrar primeira letra
+let firstLetterOfLastName = "";
+const lastName = "Flores";
+
+// atribuindo à variavel firstLetterOfLastName o valor a primeira letra do valor da variavel lastname
+firstLetterOfLastName = lastName[0];
+
+// O valor impresso será F
+console.log(firstLetterOfLastName);
+
+// atribuindo variaveis para encontrar a enésima letra, neste caso a nona
+
+const fullName = "Cintya Flores";
+const nineLetter = fullName[9];
+
+// O valor impresso será "o" porque começa a contar do zero e espaço conta
+
+console.log(nineLetter);
