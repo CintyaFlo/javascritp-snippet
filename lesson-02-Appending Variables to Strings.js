61c8d0fb03d168bc9918e7444822b704ebcd875b
CintyaFlo <flores.cd@gmail.com>
2022-09-18 17:26:19 -0300

    Appending Strings

---
+++ lesson-02-Appending Variables to Strings.js
@@ -0,0 +1,16 @@
+//atribui duas variaveis
+const someAdjective = " Difficult";
+let myStr = "Learning to code is";
+
+//concatenei as Strings de uma forma
+myStr = myStr + someAdjective;
+
+/*
+ * poderia ser de outras formas:
+ * myStr += some Adjective
+ */
+
+//duas formas diferentes de imprimeir as variaveis criadas
+
+console.log(myStr + someAdjective);
+console.log(myStr);
