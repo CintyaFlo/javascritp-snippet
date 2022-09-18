9c904e32b97791a765a8617e985ccdd33285369d
CintyaFlo <flores.cd@gmail.com>
2022-09-18 19:52:24 -0300

    find last character

--- lesson-04-bracket-notation.js
+++ lesson-04-bracket-notation.js
@@ -1,14 +1,14 @@
-// atribuindo variaveis para encontrar primeira letra
+// atribuindo variaveis para encontrar primeiro caractere
 let firstLetterOfLastName = "";
 const lastName = "Flores";
 
-// atribuindo à variavel firstLetterOfLastName o valor a primeira letra do valor da variavel lastname
+// atribuindo à variavel firstLetterOfLastName o valor a primeiro caractere do valor da variavel lastname
 firstLetterOfLastName = lastName[0];
 
 // O valor impresso será F
 console.log(firstLetterOfLastName);
 
-// atribuindo variaveis para encontrar a enésima letra, neste caso a nona
+// atribuindo variaveis para encontrar a enésimo caractere, neste caso a nona
 
 const fullName = "Cintya Flores";
 const nineLetter = fullName[9];
@@ -16,3 +16,12 @@ const nineLetter = fullName[9];
 // O valor impresso será "o" porque começa a contar do zero e espaço conta
 
 console.log(nineLetter);
+
+// atribuindo variaveis para encontrar o ultimo caractere usando [nomedavariavel.length - 1]
+
+const firstName = "Cintya";
+const lastLetter = firstName[firstName.length - 1];
+
+// O valor impresso será "a"
+
+console.log(lastLetter);
