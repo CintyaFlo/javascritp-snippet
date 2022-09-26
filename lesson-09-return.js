f433379cd36629c017e8a308600e3da21f16ffed
CintyaFlo <flores.cd@gmail.com>
2022-09-26 18:57:55 -0300

    names changed

--- lesson-09-return.js
+++ lesson-09-return.js
@@ -1,14 +1,14 @@
 // atribui um valor para uma variavel Global
 let processed = 0;
 // dentro da funçao informo que se trada de uma funçao com um parametro num que deve ter como resultado este parametro+3/5
-function processArg(num) {
+function processArgument(num) {
   return (num + 3) / 5;
 }
 
 // aqui eu altero o valor da variavel processed e informo que seu valor será igual ao resultado da funçao.
-processed = processArg(7);
+processed = processArgument(7);
 //o numero atribuido para num foi 7 e o resultado da funçao será 2, este resultado será atribuido, tmbem para a variavel processed
-console.log(processArg(7));
+console.log(processArgument(7));
 console.log(processed);
 
 let melancia = 18;
