bc9a69dbeafc6db76d19b8ed7f9759f2c76a2144
CintyaFlo <flores.cd@gmail.com>
2022-09-29 17:32:25 -0300

    if else cascade golf

--- lesson-10-if-else.js
+++ lesson-10-if-else.js
@@ -22,8 +22,9 @@ function testSize(num) {
     return "Medium";
   } else if (num < 20) {
     return "Large";
-  } else num >= 20;
-  return "Change Me";
+  } else {
+    return "Change Me";
+  }
 }
 
 testSize(7);
@@ -31,3 +32,36 @@ testSize(7);
 console.log(testSize(3)); // resultado sera Tyny
 console.log(testSize(7)); //resultado será Smal
 console.log(testSize(21)); //resultado será Change-me
+
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+  // Only change code above this line
+}
+golfScore(5, 4);
+console.log(golfScore(5, 8));
+console.log(golfScore(5, "asd"));
