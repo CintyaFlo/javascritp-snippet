8cc69f62187a0460dd4c17e8a4a16e489176226d
CintyaFlo <flores.cd@gmail.com>
2022-10-02 12:09:16 -0300

    using dot and bracket notation

---
+++ lesson-16-using-object.js
@@ -0,0 +1,14 @@
+//Defining an object
+
+const myBag = {
+  "personal-items": 4,
+  "professional-items": 6,
+  medicine: 2,
+};
+
+// Accessing an object property using a Dot Notation (.) cannot be used when the property name includes spaces or symbols
+console.log(myBag.medicine); //return 2 - the associated value of the object medicine property
+
+// Accessing an object property using a Bracket Notation []
+console.log(myBag["medicine"]); //return 2 - the associated value of the object medicine property
+console.log(myBag["personal-items"]); //return 4 - the associated value of the object personal-items property
