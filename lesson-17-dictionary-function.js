d80d167fddee8d5edda46543eb77d330d75f82af
CintyaFlo <flores.cd@gmail.com>
2022-10-02 17:24:02 -0300

    dictionary function
    Co-authored-by: Roberto Di Lillo <dilillo.roberto@gmail.com>

---
+++ lesson-17-dictionary-function.js
@@ -0,0 +1,34 @@
+function findDefinitionOf(word) {
+  const dictionary = {
+    latir: "som do cão",
+    abraçar: "pedido do amor",
+    beijar: "tocar labios",
+    pensar: "raciocinar",
+  };
+
+  return dictionary[word];
+}
+
+console.log(findDefinitionOf("latir"));
+
+function createSentenceFrom(words) {
+  let result = "";
+  const dictionary = {
+    latir: "som do cão",
+    abraçar: "pedido do amor",
+    beijar: "tocar labios",
+    pensar: "raciocinar",
+  };
+
+  const myArg = function (word) {
+    result += dictionary[word] + " ";
+  };
+
+  words.forEach(myArg);
+
+  return result;
+}
+
+console.log(createSentenceFrom(["latir", "beijar", "pensar"]));
+
+console.log(typeof [].forEach);
