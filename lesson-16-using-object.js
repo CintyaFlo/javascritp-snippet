c58691adb6aa8faae1226e2cd89a4c39f4a169a7
CintyaFlo <flores.cd@gmail.com>
2022-10-02 12:25:56 -0300

    Accessing an object property using a variable within bracket notation

--- lesson-16-using-object.js
+++ lesson-16-using-object.js
@@ -12,3 +12,9 @@ console.log(myBag.medicine); //return 2 - the associated value of the object med
 // Accessing an object property using a Bracket Notation []
 console.log(myBag["medicine"]); //return 2 - the associated value of the object medicine property
 console.log(myBag["personal-items"]); //return 4 - the associated value of the object personal-items property
+
+//Accessing an object property using a variable within bracket notation
+let propertyName = "personal-items";
+let numOfPersonalItem = myBag[propertyName];
+
+console.log(propertyName, numOfPersonalItem);
