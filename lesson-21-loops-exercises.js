d01472263c27828c83dc46d1a0ee916050a40a6c
CintyaFlo <flores.cd@gmail.com>
2022-10-03 12:18:23 -0300

    even and odd with for loop

--- lesson-21-loops-exercises.js
+++ lesson-21-loops-exercises.js
@@ -1,13 +1,13 @@
 //While - Loop
 
 // using While = the specified condition will runs until the condition be no longer true.
-const myArray = [];
+const myArray1 = [];
 let i = 7;
 
 while (i >= 0) {
-  myArray.push(i--);
+  myArray1.push(i--);
 }
-console.log(myArray); // return one array : [7, 6, 5, 4, 3, 2, 1, 0]
+console.log(myArray1); // return one array : [7, 6, 5, 4, 3, 2, 1, 0]
 
 // For - Loop
 
@@ -18,10 +18,28 @@ where a is the initialization statement, b
  and c is the final expression.
 */
 
-const myArray = [];
+const myArray2 = [];
 
-for (let i = 1; i <= 5; i++) {
-  // i can use differt form like: for (let i=1; i<6; i++) with the same result
+for (let i = 1; i <= 4; i++) {
+  // i can use differt form like: for (let i=1; i<5; i++) with the same result
+  myArray2.push(i);
+}
+console.log(myArray2); // return: [ 1, 2, 3, 4];
+
+// ODD or EVEN with For Loop
+
+//odd
+
+const myArray = [];
+for (let i = 1; i <= 10; i += 2) {
   myArray.push(i);
 }
-console.log(myArray); // return: [ 1, 2, 3, 4, 5 ];
+console.log(myArray); // return [ 1, 3, 5, 7, 9 ]
+
+//Even
+const ourArray = [];
+
+for (let i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+console.log(ourArray); //[ 0, 2, 4, 6, 8 ]
