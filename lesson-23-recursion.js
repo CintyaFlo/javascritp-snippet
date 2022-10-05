eacbea9f490e3921d983668741ae3b086f38163b
CintyaFlo <flores.cd@gmail.com>
2022-10-05 08:54:43 -0300

    sum and multiply recursion

---
+++ lesson-23-recursion.js
@@ -0,0 +1,25 @@
+/* Replace Loops using Recursion
+The recursion is an optio to substitute the loop
+this is a function that calls itself
+*/
+
+//SUM
+
+function sum(arr, n) {
+  // Only change code below this line
+  if (n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+  // Only change code above this line
+}
+//MULTIPLY
+
+function multiply(arr, n) {
+  if (n <= 0) {
+    return 1;
+  } else {
+    return multiply(arr, n - 1) * arr[n - 1];
+  }
+}
