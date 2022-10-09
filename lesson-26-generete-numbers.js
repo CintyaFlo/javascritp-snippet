5fa4f5320a2ef77c7fba26ecc227883de4a3127a
CintyaFlo <flores.cd@gmail.com>
2022-10-09 08:57:22 -0300

    aproximate numbers

--- lesson-26-generete-numbers.js
+++ lesson-26-generete-numbers.js
@@ -11,4 +11,27 @@ function randomFraction() {
 console.log(function randomFraction());
 // will generate one decimal number beetwen 0 (inclusive) ans 1 (exclusive)
 
-//Generate Random Whole Numbers
+//Generate Random Whole Numbers and aproximate to a closed integer number using Math.floor()
+function randomWholeNum() {
+    let aproxNumber = Math.floor(Math.random() * 10);
+    return aproxNumber;
+    }
+//will generate a integral number between 0 and 9, because when i multiply the Math.random() times 10, the result change from 0 until 10(exclusive)
+
+
+//i can genetate one aleatroty decimal number, aproximate it for the closed integer number inside one range of numbers
+// i wil use this formula;
+
+    Math.floor(Math.random() * (max - min + 1)) + min
+
+//EXAMPLE;
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    let intervalNumber = Math.floor(Math.random()* (myMax - myMin + 1)) + myMin;
+    return intervalNumber;
+    // Only change code above this line
+  }
+  
+  console.log(randomRange(5, 15));
+  //Ii will return one number between 5 and 14;
+
