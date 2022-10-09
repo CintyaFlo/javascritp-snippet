65077f07c9b0c3a060eafb2969c44ba04e365b52
CintyaFlo <flores.cd@gmail.com>
2022-10-09 08:33:09 -0300

    decimal numbers

---
+++ lesson-26-generete-numbers.js
@@ -0,0 +1,14 @@
+//i can generate number with Math.random() function
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
+console.log(function randomFraction());
+// will generate one decimal number beetwen 0 (inclusive) ans 1 (exclusive)
+
+//Generate Random Whole Numbers
