3c488fd1b35ddef9adeed177dcfbb882650539ee
CintyaFlo <flores.cd@gmail.com>
2022-10-09 08:32:42 -0300

    final solution

--- lesson-25-contactlist.js
+++ lesson-25-contactlist.js
@@ -133,3 +133,51 @@ function findPropOf(name, prop) {
     return "Não achei a propriedade que procurava";
   }
 }
+
+//Solution in FCC
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i];
+    if (name == contact.firstName) {
+      if (contact.hasOwnProperty(prop)) {
+        return contact[prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
