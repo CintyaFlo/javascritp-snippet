5fedd2b9d791828bc9ad637e433456db80b0d756
CintyaFlo <flores.cd@gmail.com>
2022-10-22 17:31:58 -0300

    getter and seters challenge

---
+++ ecmascript/lesson-06-getters-and-setters.js
@@ -0,0 +1,18 @@
+class Thermostat {
+  constructor(t) {
+    this._temperature = t;
+  }
+
+  get temperature() {
+    return (5 / 9) * (this._temperature - 32);
+  }
+  set temperature(newTemp) {
+    this._temperature = (newTemp * 9) / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
