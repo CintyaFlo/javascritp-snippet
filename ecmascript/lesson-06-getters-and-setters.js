87eca25df1db266065ee58722e20abfebb9e30cf
CintyaFlo <flores.cd@gmail.com>
2022-10-27 14:00:47 -0300

    termostat new observations

--- ecmascript/lesson-06-getters-and-setters.js
+++ ecmascript/lesson-06-getters-and-setters.js
@@ -1,18 +1,21 @@
 class Thermostat {
-  constructor(t) {
-    this._temperature = t;
+  constructor(farenheit) {
+    this._farenheit = farenheit;
   }
 
   get temperature() {
-    return (5 / 9) * (this._temperature - 32);
+    return (5 / 9) * (this._farenheit - 32);
   }
-  set temperature(newTemp) {
-    this._temperature = (newTemp * 9) / 5 + 32;
+  set temperature(celsius) {
+    this._farenheit = (celsius * 9) / 5 + 32;
   }
 }
 // Only change code above this line
 
 const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
+//thermos.temperature = 26;
+//temp = thermos.temperature; // 26 in Celsius
+
+console.log(new Thermostat(76));
+console.log(temp);
