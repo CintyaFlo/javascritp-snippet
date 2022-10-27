f6eb1065ba9fc29828cfd85b0b6c374c05e4cc9d
CintyaFlo <flores.cd@gmail.com>
2022-10-27 20:23:30 -0300

    Match Everything But Letters and Numbers

--- regular-expressions/lesson02-shorthand-character-classes.js
+++ regular-expressions/lesson02-shorthand-character-classes.js
@@ -1,4 +1,4 @@
-// Match All Letters and Numbers
+// Match All Letters and Numbers - \w
 
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /\w/g; /* the \w is a shorthand to /[A-Za-z0-9_]+/ 
@@ -7,3 +7,11 @@ the letter g is the global search flag, to be able to extract a pattern more tha
 let result = quoteSample.match(alphabetRegexV2).length;
 // above we see the match found every element, and the .lenght is able to count any character
 console.log(result); // result 31.
+
+//Match Everything But Letters and Numbers \W
+
+let quoteSample1 = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // \W is the shorthand to [^A-Za-z0-9_]
+let result1 = quoteSample1.match(nonAlphabetRegex).length;
+
+console.log(result1);
