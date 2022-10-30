0aa8314e94c3072f83f9b806347a605d71eececd
CintyaFlo <flores.cd@gmail.com>
2022-10-30 16:57:05 -0300

    add value to an object

---
+++ data Structures/lesson-01-object-add-key-value-pairs.js
@@ -0,0 +1,13 @@
+const tekkenCharacter = {
+  player: "Hwoarang",
+  fightingStyle: "Tae Kwon Doe",
+  human: true,
+};
+let hair = "hair_color";
+tekkenCharacter.origin = "South Korea";
+tekkenCharacter[hair] = "dyed orange";
+/*inside the bracket notation its nessessary 
+to put or one variable, or on string, in theese lest case, " is mandatory"
+*/
+
+console.log(tekkenCharacter);
